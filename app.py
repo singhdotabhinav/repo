0eff91c2bafb75f9c1a1fb03082bacdd421a366d
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2023-12-12 13:24:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ def main():
 
     st.header("Work Experience - Cimpress India Pvt. Ltd.")
     st.write(
-        "Software Developer, Bangalore, India\n"
-        "Aug 2021 - Present\n"
-        "Orchestrated the migration of data storage to AWS S3, optimizing data retrieval time by 40%..."
+        "Software Developer | Bangalore, India | Aug 2021 - Present\n"
+        "• Orchestrated migration to AWS S3, optimizing data retrieval time by 40%...\n"
+        "• Implemented AWS Step Functions to streamline workflows, reducing processing time by 50%...\n"
+        "• Experience in deploying Fargate and EC2 tasks from Docker containers..."
         # (Include all other work experience details)
     )
 
@@ -17,16 +18,23 @@ def main():
 
     st.header("Project Experience - NoChinano.com")
     st.write(
-        "Co-Founder and Developer, Tumakuru, India\n"
+        "Co-Founder and Developer | Tumakuru, India\n"
         "Graduation Date: Aug 2021\n"
-        "Inspired by India's endeavors to help India boycott Chinese brands..."
+        "Inspired by India's endeavors to boycott Chinese brands, developed a website...\n"
+        "• More than 1k active users\n"
+        "• Fetching real-time data from Firebase for products and Amazon API for price updates..."
         # (Include all other project details)
     )
 
     st.header("Skills")
     st.write(
-        "Python, AWS (Step-Functions, S3, Lambda, EC2, Fargate, DynamoDB), "
-        "Serverless Computing, CI/CD, Snowflake, Docker, Pytest"
+        "• Python\n"
+        "• AWS (Step-Functions, S3, Lambda, EC2, Fargate, DynamoDB)\n"
+        "• Serverless Computing\n"
+        "• CI/CD\n"
+        "• Snowflake\n"
+        "• Docker\n"
+        "• Pytest"
     )
 
 if __name__ == "__main__":
