73d703d2b42a381f9cc492fccb92b63d56f6d1b6
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2023-12-12 13:16:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+# app.py
+import streamlit as st
+
+def main():
+    st.title("Simple Streamlit App")
+    st.write("Hello, this is a simple Streamlit app!")
+
+if __name__ == "__main__":
+    main()
